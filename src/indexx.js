85f6706eb4c29754b759635f5a95aed52f205442
João Girardi <joao.girardi@pontosys.com>
2022-06-06 20:27:08 -0300

    Arquivo final

--- src/indexx.js
+++ src/indexx.js
@@ -5,3 +5,7 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send("Atividade pré-avaliação!");
 })
+
+app.listen(port, () => {
+    console.log('Rodando no site "Heroku", na porta 3000');
+})
